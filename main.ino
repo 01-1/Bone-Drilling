6c6cc3d49d18cd6dc14c07f6f5bb64d5c328aba1
Unum <33045716+etaui@users.noreply.github.com>
2020-12-10 22:42:44 -0500

    refactoring

--- main.ino
+++ main.ino
@@ -2,13 +2,14 @@
 
 const byte RPM_SENSOR_PIN = 2;
 const byte DRILL_PIN = 3;
-const double pulse_threshold = 0.99;
+const double PULSE_THRESHOLD = 0.99;
+const long TIMEOUT_THRESHOLD = 2000000;
 
 volatile unsigned long t_pulse_started_volatile = 0;
 volatile unsigned long t_pulse_duration_volatile = 0;
 unsigned long t_pulse_started = 0;
 unsigned long t_pulse_duration = 0;
-unsigned long t_pulse_duration_l = 2000000;
+unsigned long t_pulse_duration_l = TIMEOUT_THRESHOLD;
 
 /* Not using rpm, using the inverse (rotation period) instead.
 Easier to calculate, and arithmetic mean of periods (which would be the harmonic mean of rpm) 
@@ -34,15 +35,15 @@ void loop() {
   t_pulse_duration = t_pulse_duration_volatile;
   interrupts();
 
-  if (((micros() - t_pulse_started) > 2000000) && timeout == 0 && newpulse == 0) {
+  if (((micros() - t_pulse_started) > TIMEOUT_THRESHOLD) && timeout == 0 && newpulse == 0) {
     timeout = 1;
-    t_pulse_duration = 2000000;
+    t_pulse_duration = TIMEOUT_THRESHOLD;
   };
 
   if (timeout == 0 && newpulse) {
     Serial.print(t_pulse_duration); // for data collection stage only, may affect timing significantly
     newpulse = 0;
-    if (static_cast<double>(t_pulse_duration_l) / t_pulse_duration < pulse_threshold) {
+    if (static_cast<double>(t_pulse_duration_l) / t_pulse_duration < PULSE_THRESHOLD) {
       analogWrite(DRILL_PIN, 0);
     }
     
@@ -56,5 +57,11 @@ void ISR_sensor() {
   t_pulse_started_volatile = micros_temp;
   timeout = 0;
   newpulse = 1;
-
+  // possible timing issue: if ISR_sensor() activates twice before loop() is called: this causes data loss.
 }
+
+/* TODO:
+ * Add force sensor
+ * improve data collection by using a list (vector/array)?
+ * 
+ */
