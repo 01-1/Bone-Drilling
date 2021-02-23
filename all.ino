33cccea0b956cc6a0e9fda191ce79f537e9974bd
Unum <33045716+etaui@users.noreply.github.com>
2021-02-23 00:22:32 -0500

    linear actuator

--- all.ino
+++ all.ino
@@ -1,27 +1,44 @@
-#include "Adafruit_MLX90614.h"
-#include <cstdint>
+#include <Adafruit_MLX90614.h>
+#include <HX711.h>
+#include <L298N.h>
 
-enum class stage {
-    before_drilling,
-    first_cortical,
-    trabecular,
-    second_cortical
+enum class Stage {
+    BEFORE_DRILLING,
+    INITIAL_FIRST_CORTICAL,
+    MAIN_FIRST_CORTICAL,
+    TRABECULAR,
+    SECOND_CORTICAL,
+    IDLE
 };
 
-stage current_stage = stage::before_drilling;
+enum class Scenario {
+    STOP_TRABECULAR,
+    FULL
+};
+
+Stage stage = Stage::BEFORE_DRILLING;
+Scenario scenario = Scenario::FULL;
 
 // Pin constants
 namespace Pin {
-    const uint8_t DRILL = 3;
-    const uint8_t LINEAR_ACTUATOR = 4; // check analog and digital pins
+    using p = const uint8_t;
+    p LOADCELL_DOUT_PIN = 1;
+    p LOADCELL_SCK_PIN = 2;
+    p LINEAR_ACTUATOR_EN = 3;
+    p LINEAR_ACTUATOR_IN1 = 4;
+    p LINEAR_ACTUATOR_IN2 = 5;
 }
 
 // Constants
-namespace Threshold {
-    const float START_VOLTAGE = 0.1f;
-    const double CT_FACTOR = 0.5;
-    const double TC_FACTOR = 2.0;
-}
+
+const float STARTING_VOLTAGE_THRESHOLD = 0.1f;
+const double ENTER_TRABECULAR_THRESHOLD_FACTOR = 0.5;
+const double ENTER_CORTICAL_THRESHOLD_FACTOR = 2.0;
+
+// 10 mm/s full speed
+const unsigned short INITIAL_SPEED = 13; // 0.5 mm/s
+const unsigned short CORTICAL_SPEED = 26; // 1 mm/s
+const unsigned short TRABECULAR_SPEED = 69; // 0.5 mm/s
 
 // Variables
 double cortical_total = 0;
@@ -31,22 +48,35 @@ double trabecular_total = 0;
 uint32_t trabecular_len = 0;
 
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+HX711 loadcell;
+L298N linear_actuator(Pin::LINEAR_ACTUATOR_EN, Pin::LINEAR_ACTUATOR_IN1, Pin::LINEAR_ACTUATOR_IN2);
 
+unsigned long timer_end; // 70 minutes overflow
 
 void setup() {
     Serial.begin(9600);
     mlx.begin();
+    
+    // TODO: Add serial scenario input
 
-    pinMode(Pin::DRILL, OUTPUT);
-    pinMode(Pin::LINEAR_ACTUATOR, OUTPUT);
-    analogWrite(Pin::DRILL, 255);
-    analogWrite(Pin::LINEAR_ACTUATOR, 255);
+    pinMode(Pin::LINEAR_ACTUATOR_EN, OUTPUT);
+    pinMode(Pin::LINEAR_ACTUATOR_IN1, OUTPUT);
+    pinMode(Pin::LINEAR_ACTUATOR_IN2, OUTPUT);
 
+    pinMode(Pin::LOADCELL_DOUT_PIN, INPUT);
+    pinMode(Pin::LOADCELL_SCK_PIN, INPUT);
+
+    loadcell.begin(Pin::LOADCELL_DOUT_PIN, Pin::LOADCELL_SCK_PIN);
+    loadcell.set_scale();
+    loadcell.tare();
+    
+    linear_actuator.setSpeed(CORTICAL_SPEED);
 }
 
 void stopDrill() {
-    analogWrite(Pin::DRILL, 0);
-    analogWrite(Pin::LINEAR_ACTUATOR, 0);
+    // linear_actuator.stop();
+    linear_actuator.backward();
+    stage = Stage::IDLE;
 }
 
 bool update(float force_voltage, double& total, uint32_t& len, double factor) {
@@ -56,29 +86,44 @@ bool update(float force_voltage, double& total, uint32_t& len, double factor) {
 }
 
 void loop() {
-    float force_voltage = 0; // modify
+    float force_voltage = loadcell.get_units(10); // modify
     // library for HX711 uses float
 
-    switch (current_stage) {
-        case stage::before_drilling:
-            if (force_voltage > Threshold::START_VOLTAGE) {
-                current_stage = stage::first_cortical;
+    switch (stage) {
+        case Stage::BEFORE_DRILLING:
+            if (force_voltage > STARTING_VOLTAGE_THRESHOLD) {
+                stage = Stage::INITIAL_FIRST_CORTICAL;
+                timer_end = micros() + 2000000;
             }
             break;
-        case stage::first_cortical:
-            if (update(force_voltage, cortical_total, cortical_len, Threshold::CT_FACTOR)) {
-                current_stage = stage::trabecular;
+        case Stage::INITIAL_FIRST_CORTICAL:
+            if (micros() > timer_end) {
+                stage = Stage::MAIN_FIRST_CORTICAL;
             }
             break;
-        case stage::trabecular:
-            if (update(force_voltage, trabecular_total, trabecular_len, Threshold::TC_FACTOR)) {
-                current_stage = stage::second_cortical;
+        case Stage::MAIN_FIRST_CORTICAL:
+            if (update(force_voltage, cortical_total, cortical_len, ENTER_TRABECULAR_THRESHOLD_FACTOR)) {
+                if (scenario == Scenario::STOP_TRABECULAR) {
+                    stopDrill();
+                } else {
+                    stage = Stage::TRABECULAR;
+                    linear_actuator.setSpeed(TRABECULAR_SPEED);
+                }
             }
             break;
-        case stage::second_cortical:
-            if (update(force_voltage, cortical_total, cortical_len, Threshold::CT_FACTOR)) {
+        case Stage::TRABECULAR:
+            if (update(force_voltage, trabecular_total, trabecular_len, ENTER_CORTICAL_THRESHOLD_FACTOR)) {
+                stage = Stage::SECOND_CORTICAL;
+                linear_actuator.setSpeed(CORTICAL_SPEED);
+            }
+
+            break;
+        case Stage::SECOND_CORTICAL:
+            if (update(force_voltage, cortical_total, cortical_len, ENTER_TRABECULAR_THRESHOLD_FACTOR)) {
                 stopDrill();
             }
+            break;
+        case Stage::IDLE:
             break;
     }
 
